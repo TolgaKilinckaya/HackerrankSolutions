3f385b23339b7ea89798bd1a112e3f61287196ef
TolgaKilinckaya <119072606+TolgaKilinckaya@users.noreply.github.com>
2023-02-12 22:26:31 +0300

    Create Hello-World-in-C.c

---
+++ C/Hello-World-in-C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("Hello, World!\n%s",s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
